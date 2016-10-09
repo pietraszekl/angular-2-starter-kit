38aff5e57cc8afd497fd63431b2de5011f30a04a
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2016-10-09 11:41:24 +0100

    creating new component for user profile

---
+++ app/users/user-profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { User } from '../shared/models/user';
+
+@Component({
+  selector: 'user-profile',
+  template: `
+  <div  class="jumbotron">
+    <div *ngIf="user">
+      <h1>{{ message }} {{user.name}}</h1>
+      <input class="form-control" [(ngModel)]="user.name"/>
+    </div>
+    <div *ngIf="!user">
+      <h2><span class="glyphicon glyphicon-hand-left"></span> Choose user</h2>
+    </div>
+  </div>
+  `
+})
+
+export class UserProfileComponent {
+  @Input() user: User;
+}
