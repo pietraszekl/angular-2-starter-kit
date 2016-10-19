454cc295139eeb4f61df65a51dcfe5c7f02d57ae
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2016-10-19 21:23:09 +0100

    refactoing about user component

--- app/about/about-user.component.ts
+++ app/about/about-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../shared/models/user';
 import { UserService } from '../shared/services/user.service';
 
@@ -11,6 +11,7 @@ import { UserService } from '../shared/services/user.service';
     }
   `],
   template: `
+    <a (click)="goBack()" class="btn btn-sm">Go Back</a>
     <div *ngIf="user" class="jumbotron text-center">
     <h1>{{user.name}} ({{user.username}})</h1>
     <img [src]="user.avatar" class="img-responsive img-circle" alt="">
@@ -21,11 +22,18 @@ import { UserService } from '../shared/services/user.service';
 export class AboutUserComponent implements OnInit{
   user: User;
 
-  constructor(private route: ActivatedRoute, private userService: UserService){}
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private router: Router
+  ){}
 
   ngOnInit(){
     let username = this.route.snapshot.params['username'];
-
     this.userService.getUser(username).then(user => this.user = user );
+  }
+
+  goBack(){
+    this.router.navigate(['/about'])
   }
 }
